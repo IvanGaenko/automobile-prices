e4ae92014cec06ab28b101de8cf095edcfeeada3
IvanGaenko <gaenko37@gmail.com>
2023-08-11 00:38:52 +0300

    Checking error6

--- src/lib/getClassTier.js
+++ src/lib/getClassTier.js
@@ -11,7 +11,7 @@ export default async function getClassTier() {
     headless: true,
     executablePath:
       process.env.NODE_ENV === "production"
-        ? "/usr/bin/chromium-browser"
+        ? "/usr/bin/google-chrome-stable"
         : puppeteer.executablePath(),
   });
 
