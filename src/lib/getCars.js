d172d117d2901f8661a0ae2969156c905a3125c4
IvanGaenko <gaenko37@gmail.com>
2023-08-19 01:17:51 +0300

    Fix cars query params 13

--- src/lib/getCars.js
+++ src/lib/getCars.js
@@ -5,20 +5,21 @@ export default async function getCars(search) {
   let carsData = [];
   let webPage = 0;
   let pageSize = 100;
-
+  console.log("search in getCars", search);
   function parsingNumber(str) {
     return parseInt(str.split(" ").join(""), 10);
   }
 
   async function fetchCars() {
+    console.log("start fetch cars");
     const { data } = await axios.get(
       `https://auto.ria.com/uk/search/?indexName=auto,order_auto,newauto_search&categories.main.id=1&price.currency=1&abroad.not=0&custom.not=1&size=${pageSize}&damage.not=1&sort[0].order=dates.created.desc&page=${webPage}&${search}`
     );
-
+    console.log("inside", data);
     const dom = new JSDOM(data);
 
     const carList = dom.window.document.querySelectorAll("section.ticket-item");
-
+    console.log("carList.length", carList.length);
     if (carList.length > 0) {
       const newCars = Array.from(carList).map((car) => {
         const name = car.querySelector("span.blue.bold").innerHTML.trim();
@@ -41,12 +42,13 @@ export default async function getCars(search) {
       webPage++;
 
       if (webPage < 5) {
+        console.log("again");
         await fetchCars();
       }
     }
   }
 
   await fetchCars();
-
+  console.log("carsData", carsData);
   return carsData;
 }
